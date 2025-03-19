433aa172203a0808f0964a7965e00aca46b92f62
soongfs <1415022760@qq.com>
2025-03-19 14:27:49 +0800

    Initial commit: project setup

---
+++ src/main/java/com/dsproject/triphub/TriphubApplication.java
@@ -0,0 +1,13 @@
+package com.dsproject.triphub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriphubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriphubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsproject/triphub/TriphubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsproject.triphub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriphubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
